9f5da4e8e3b7969710591762f2428bd17a252097
Nastaran Sadra <nastaransadra@yahoo.com>
2023-07-26 04:16:46 +0400

    api for login,logout,register,change password, reset password

---
+++ account/urls.py
@@ -0,0 +1,16 @@
+from .views import RegisterAPI, ChangePasswordView,LoginAPI
+from django.urls import path, include
+
+from knox import views as knox_views
+
+
+urlpatterns = [
+    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
